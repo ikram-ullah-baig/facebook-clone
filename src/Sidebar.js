7a078b32bf7b21aedb63fdbbc176d28b4ad670c3
ikram-ullah-baig <ikramullahgmt91@gmail.com>
2021-01-14 23:42:40 +0500

    context API, reducer and state hooks used

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -8,14 +8,14 @@ import ChatIcon from "@material-ui/icons/Chat";
 import StorefrontIcon from "@material-ui/icons/Storefront";
 import VideoLibraryIcon from "@material-ui/icons/VideoLibrary";
 import { ExpandMoreOutlined } from "@material-ui/icons";
+import { useStateValue } from "./StateProvider";
 
 function Sidebar() {
+    const [{ user }, dispatch] = useStateValue();
+
     return (
         <div className="sidebar">
-            <SidebarRow 
-                src="https://avatars0.githubusercontent.com/u/64208875?s=60&v=4" 
-                title="Ikram Ullah Baig"
-            />
+            <SidebarRow src={user.photoURL} title={user.displayName} />
             <SidebarRow 
                 Icon= {LocalHospitalIcon}
                 title="COVID-19 Information Center"
--- src/firebase.js
+++ src/firebase.js
@@ -8,3 +8,11 @@ const firebaseConfig = {
     messagingSenderId: "577341415485",
     appId: "1:577341415485:web:265e54db436327c40cca54"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  export { auth, provider };
+  export default db;
